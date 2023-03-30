963da6f13430b48c162105b6775c53c5f1f362a5
Replit user <>
2023-03-30 16:02:29 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -17,9 +17,9 @@ ArrayList *createList(void) {
 
   if (Lista == NULL) return NULL;
 
-  //Lista->capacity = 2;
+  Lista->capacity = 2;
   Lista->size = 0;
-  Lista->data = (void **) malloc(sizeof(void *) * Lista->capacity);
+  Lista->data = (void *) malloc(sizeof(void *) * Lista->capacity);
  
   return Lista;
 }
