586e2fa71c67444551f459e029b8c5f0c0fbe8e0
Replit user <>
2023-04-02 00:05:39 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -11,24 +11,46 @@ typedef struct ArrayList {
 } ArrayList;
 
 ArrayList *createList(void) {
+
+  ArrayList *Lista = NULL;
+  Lista = (ArrayList *) malloc(sizeof(ArrayList));
+
+  if (Lista == NULL) return NULL;
+
+  Lista->capacity = 2;
+  Lista->size = 0;
+  Lista->data = (void **) malloc(sizeof(void *) * Lista->capacity);
  
-  return NULL;
+  return Lista;
 }
 
 void append(ArrayList * l, void * data){
   
+  if (l->size == l->capacity)
+  {
+    l->capacity *= 2;
+    l->data = (void **) realloc(l->data, (l->capacity) * sizeof(void *));
+    l->data[l->size] = data;
+    l->size += 1;
+  }
+  else
+  {
+    l->data[l->size] = data;
+    l->size += 1;
+  }
 
 }
 
 void push(ArrayList * l, void * data, int i){
- int j;
+
+  int j;
   
   if ( i > l->size) return ;
 
   if (l->capacity == l->size)
   {
     l->capacity *= 2;
-    //l->data = realloc(l->data, sizeof(void *) * l->capacity);
+    //l->data = (void **) realloc(l->data, (l->capacity) * sizeof(void *));
   }
 
   
@@ -40,15 +62,60 @@ void push(ArrayList * l, void * data, int i){
     l->data[i] = data;
       
     l->size += 1;
+  
+  
+
 }
 
 void* pop(ArrayList * l, int i){
-  
+  void *valorEliminado = NULL;
+  int j;
+
+  if (l->size == 0)
+  {
     return NULL;
+  }
+  
+  if( i >=0 )
+  {
+    valorEliminado = l->data[i];
+
+    for (j = i ; j < l->size ; j++ )
+      {
+        l->data[j] =l->data[j + 1];
+      }
+    l->size = (l->size - 1);
+  }
+  else
+  {
+    valorEliminado = l->data[l->size + i];
+
+    for (j = (l->size + i) ; j < l->size ; j++)
+      {
+        l->data[j] = l->data[j + 1];
+      }
+    l->size = (l->size - 1);
+  }
+  
+    return valorEliminado;
 }
 
 void* get(ArrayList * l, int i){
 
+  if ( i >= l->size)
+  {
+    return NULL;
+  }
+
+  if (i >= 0)
+  {
+    return l->data[i];
+  }
+  else
+  {
+    return l->data[l->size + i];
+  }
+  
     return NULL;
 }
 
@@ -59,6 +126,9 @@ int get_size(ArrayList * l){
 //remove elements
 void clean(ArrayList * l){
 
-  
+  l->capacity = 2;
+  l->size = 0;
+
+  l->data = (void **) realloc(l->data, l->capacity * sizeof(void *));
     
 }
